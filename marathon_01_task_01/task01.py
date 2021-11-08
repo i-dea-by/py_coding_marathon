50039bdb27b543d0eeaa90e4d383351f686ec7db
egor daniloff <i@dea.by>
2021-11-08 19:51:00 +0300

    * тайпхинтинги в задаче №1 * вывод результатов
    + добавил
    - убрал/удалил
    * исправил/подправил

--- marathon_01_task_01/task01.py
+++ marathon_01_task_01/task01.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 
-def hamming_distance(str1, str2: str) -> int:
+def hamming_distance(str1: str, str2: str) -> int:
     result = 0
     for x in range(len(str1)):
         if str1[x] != str2[x]:
@@ -9,15 +9,15 @@ def hamming_distance(str1, str2: str) -> int:
     return result
 
 
-def hamming_distance_v2(str1, str2: str) -> int:
+def hamming_distance_v2(str1: str, str2: str) -> int:
     return len([str1[x] for x in range(len(str1)) if str1[x] != str2[x]])
 
 
-def hamming_distance_v3(str1, str2: str) -> int:
+def hamming_distance_v3(str1: str, str2: str) -> int:
     return list(map(lambda x, y: x == y, str1, str2)).count(False)
 
 
-def hamming_distance_enum(str1, str2: str) -> int:
+def hamming_distance_enum(str1: str, str2: str) -> int:
     """ """
     result = 0
     for x, char in enumerate(str1):
@@ -25,19 +25,13 @@ def hamming_distance_enum(str1, str2: str) -> int:
     return result
 
 
-def hamming_distance_red(str1, str2: str) -> int:
+def hamming_distance_red(str1: str, str2: str) -> int:
     return reduce(lambda a, b: a + b, map(lambda x, y: x != y, str1, str2))
 
 
 if __name__ == '__main__':
-    print(hamming_distance("abcd0", "bcdef"))  # 5
-    print(hamming_distance_v2("abcd0", "bcdef"))  # 5
-    print(hamming_distance_v3("abcd0", "bcdef"))  # 5
-    print(hamming_distance_enum("abcd0", "bcdef"))  # 5
-    print(hamming_distance_red("abcd0", "bcdef"))  # 5
-    print()
-    print(hamming_distance("abcde", "abcd1"))  # 1
-    print(hamming_distance_v2("abcde", "abcd1"))  # 1
-    print(hamming_distance_v3("abcde", "abcd1"))  # 1
-    print(hamming_distance_enum("abcde", "abcd1"))  # 1
-    print(hamming_distance_red("abcde", "abcd1"))  # 1
+    print(f'{hamming_distance("abcd0", "bcdef")=}')  # 5
+    print(f'{hamming_distance_v2("abcd0", "bcdef")=}')  # 5
+    print(f'{hamming_distance_v3("abcd0", "bcdef")=}')  # 5
+    print(f'{hamming_distance_enum("abcd0", "bcdef")=}')  # 5
+    print(f'{hamming_distance_red("abcd0", "bcdef")=}')  # 5
